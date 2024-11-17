62fc2347972280213b6f87d0089b5df2ec5a442f
bruno maruya <brun0yuj1ym02@gmail.com>
2024-11-17 20:50:48 +0900

    feat: updat searchbar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,15 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/16/solid";
 import React from "react";
 
 export default function SearchBar() {
   return (
-    <div>
+    <div className=" mt-10 bg-[#f0f0f0] flex items-center rounded-2xl p-3 gap-2">
+      <MagnifyingGlassIcon className="w-6 h-6 text-gray-500 " />
       <input
-        className="w-full h-full bg-[#f0f0f0] mt-10 rounded-2xl p-3"
+        className="w-full h-full bg-transparent focus:outline-none  "
         placeholder="Search for notes"
-      />
+        type="search"
+      ></input>
     </div>
   );
 }
