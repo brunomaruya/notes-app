732517c667781d98479dfe70a32c958387fa8e80
bruno maruya <brun0yuj1ym02@gmail.com>
2024-11-18 16:17:52 +0900

    feat: add NotesContainer component

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import CategoryFilter from "./CategoryFilter";
+import NotesContainer from "./NotesContainer";
 
 export default function Container() {
   return (
     <div className="p-5 h-[844px] w-[390px] bg-white rounded-2xl flex flex-col gap-5">
       <SearchBar />
       <CategoryFilter />
+      <NotesContainer />
     </div>
   );
 }
---
+++ src/components/NotesContainer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function NoteCard({ header, body }) {
+  return (
+    <div className="bg-[#d9e8fc] rounded-2xl p-5 mb-3 overflow-hidden">
+      <h1>{header}</h1>
+      <p className="text-sm ">{body}</p>
+    </div>
+  );
+}
+export default function NotesContainer() {
+  return (
+    <div className="columns-2 gap-3 ">
+      <NoteCard header="Note1" body="Body1" />
+      <NoteCard
+        header="Note1"
+        body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe doloremque, neque eveniet est dolor nam praesentium unde et vero ab nihil aliquid doloribus deserunt perferendis quam earum perspiciatis dolores alias?"
+      />
+      <NoteCard
+        header="Note1"
+        body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe doloremque, neque eveniet est dolor nam praesentium unde et vero ab nihil aliquid doloribus deserunt perferendis quam earum perspiciatis dolores alias?"
+      />
+      <NoteCard header="Note1" body="Body1" />
+    </div>
+  );
+}
