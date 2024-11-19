f8bfb256cc767ba0eeee8168eee60d092f57977e
bruno maruya <brun0yuj1ym02@gmail.com>
2024-11-19 11:13:52 +0900

    feat:update Note.jsx

--- src/pages/Note.jsx
+++ src/pages/Note.jsx
@@ -1,8 +1,53 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import Container from "../components/Container";
 
 export default function Note() {
   const { noteId } = useParams();
-  return <Container>{noteId}</Container>;
+  const [titleValue, setTitleValue] = useState("");
+  const [bodyValue, setBodyValue] = useState("");
+  const titleRef = useRef();
+  const bodyRef = useRef();
+  const handleTitleInput = (e) => {
+    const textarea = e.target;
+    textarea.style.height = "auto";
+    textarea.style.height = textarea.scrollHeight + "px";
+    setTitleValue(e.target.value);
+  };
+  const handleBodyInput = (e) => {
+    const textarea = e.target;
+    textarea.style.height = "auto";
+    textarea.style.height = textarea.scrollHeight + "px";
+    setBodyValue(e.target.value);
+  };
+
+  const handleKeyDown = (e, nextRef) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      nextRef.current?.focus();
+    }
+  };
+  return (
+    <Container>
+      <header></header>
+      <textarea
+        onKeyDown={(e) => handleKeyDown(e, bodyRef)}
+        rows="1"
+        onChange={handleTitleInput}
+        autoFocus
+        className="text-2xl   font-bold focus:outline-none  "
+        style={{ resize: "none", overflow: "hidden" }}
+        value={titleValue}
+        ref={titleRef}
+      />
+      <textarea
+        onChange={handleBodyInput}
+        ref={bodyRef}
+        autoFocus
+        className="text-md  focus:outline-none  "
+        style={{ resize: "none", overflow: "hidden" }}
+        value={bodyValue}
+      />
+    </Container>
+  );
 }
